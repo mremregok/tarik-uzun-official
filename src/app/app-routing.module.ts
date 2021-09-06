b24103d92464b9e02eb162e2cf8e5791e00a6802
Emre Gök <mremregok@gmail.com>
2021-09-07 01:32:58 +0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnasayfaComponent } from './anasayfa/anasayfa.component';
+
+const routes: Routes = [
+  { path: 'anasayfa', component: AnasayfaComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
